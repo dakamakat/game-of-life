219ea173bc5c9f14252e0c6bf8126517e3711596
dakamakat <gazon-ozon@yandex.ru>
2024-04-19 20:02:50 +0200

    feat(styling/ui): update ui

---
+++ app/ui.js
@@ -0,0 +1,23 @@
+import ButtonComponent from './buttonComponent.js';
+
+export function initUI(state) {
+
+    initPauseButton((function() {
+        if (state.isPaused) {
+            state.play();
+            this.textContent = "⏸";
+        }
+        else {
+            state.pause();
+            this.textContent = "▶";
+        }
+
+        state.isPaused = !state.isPaused;
+    }));
+}
+
+function initPauseButton(handleClick) {
+    const button = new ButtonComponent("play-pause", '⏸', handleClick);
+    button.init();
+}
+
