85abc852dec61d26289df7b8fafe682734103398
dakamakat <gazon-ozon@yandex.ru>
2024-05-11 12:06:13 +0200

    feat(canvas): reduce cell draw loops

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 import __wbg_init, { Universe } from "../pkg/wasm_game_of_life.js";
 import State from "./state.js";
+import FPS from "./fps.js";
 import { drawGrid, drawCells, createCanvas } from "./grid.js";
 import { initUI } from "./ui.js";
 
@@ -7,6 +8,7 @@ const wasm = await __wbg_init();
 const memory = wasm.memory;
 
 let state = new State(false);
+let fps = new FPS();
 
 // Construct the universe, and get its width and height.
 const universe = Universe.new(1, 100, 80);
@@ -19,6 +21,9 @@ const canvas = createCanvas(height, width);
 const ctx = canvas.getContext('2d');
 
 const renderLoop = () => {
+
+    fps.render();
+
     universe.tick();
 
     drawGrid(ctx, height, width);
